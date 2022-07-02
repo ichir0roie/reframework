b3f9b5259cd4f99b6e05f913262c4b662176455c
ichir0roieHome <ichir0roie.com>
2022-07-02 15:58:18 +0900

    update move!

--- autorun/playerMaxStatus.lua
+++ autorun/playerMaxStatus.lua
@@ -33,76 +33,75 @@ function getSingletonField(singleton, name)
 	return sdk.get_native_field(singletonRef, typedef, name)
 end
 
-function getCurrentPlayer()
-	-- local inputManager = getSingletonData("snow.StmInputManager")
-	-- local inGameInputDevice = getSingletonField(inputManager, "_InGameInputDevice")
-	-- local playerInput = sdk.get_native_field(inGameInputDevice, getType("snow.StmInputManager.InGameInputDevice"), "_pl_input")
-	-- local weapon = sdk.get_native_field(playerInput, getType("snow.StmPlayerInput"), "RefPlayer")
-	-- return weapon
+local quest_started=false
+local hunter_initialized=false
 
-	local playerManager=sdk.get_managed_singleton('snow.player.PlayerManager')
-	local playerList=playerManager:get_field('PlayerList')
-	return playerList[0]
-end
-
+local playerManager=nil
+local playerList=nil
 local hunter=nil
+local playerData=nil
 
-function setup_hunter()
-	print("setup hunter")
-	hunter=getCurrentPlayer()
-	
-end
-
-function on_post_update(r)
-	if hunter ==nil then
-		print("hunter is nil.")
-		setup_hunter()
-		return false
+function updateHunterInfo()
+	-- print('update')
+	-- if hunter ~=nil then
+	-- 	return tr
+	-- end
+	if hunter_initialized then
+		return
 	end
 
-	local playerData=hunter:get_field("_refPlayerData")
+	print("update hunter")
+	if pcall(setHunter) then
+	else
+		print("error hunter setup")
+		hunter_initialized=false
+		return
+	end
 	
+	if hunter ==nil then
+		hunter_initialized=false
+		return
+	end
+	
+	playerData=hunter:get_field("_refPlayerData")
+	
+	setup_wire()
+	-- override_skill()
+
+	hunter_initialized=true
+end
+
+function setHunter()
+	playerManager=sdk.get_managed_singleton('snow.player.PlayerManager')
+	playerList=playerManager:get_field('PlayerList')		
+	hunter=playerList[0] --todo findmasterplayer?
+end
+
+function notActionStatus()
+	-- if not runActionMod or hunter ==nil or not quest_started then
+	if not hunter_initialized or not quest_started then
+		return true
+	end
+	return false
+end
+
+function updateStatus()
+	if not hunter_initialized then
+		updateHunterInfo()
+	end
+
 	playerData:set_field("_Attack",1000)
 	playerData:set_field("_Defence",50)
-	-- return r
-end
 
-local test_done=true
-
-function on_post_vital(r)
-
-	if test_done == false then
-		test()
-		test_done=true
-		print(test_done)
-	end
-
-	if hunter ==nil then
-		print("hunter is nil.")
-		setup_hunter()
-		return false
-	end
-
-	local playerData=hunter:get_field("_refPlayerData")
-	playerData:set_field("_stamina",5700)
+	playerData:set_field("_vitalMax",200)
+	playerData:set_field("_vitalKeep",200)
+	playerData:set_field("_r_Vital",200)
+	
 	playerData:set_field("_staminaMax",5700)
-	playerData:set_field("_vitalMax",150)
-	playerData:set_field("_vitalKeep",150)
-	playerData:set_field("_r_Vital",150)
-	-- return r
+	playerData:set_field("_stamina",5700)
 end
 
-function test()
-	print("test")
-	if hunter ==nil then
-		print("hunter is nil.")
-		setup_hunter()
-		return false
-	end
-	local playerData=hunter:get_field("_refPlayerData")
-	print(playerData)
-	override_skill()
-end
+
 
 function view_info(o)
 	if o == nil then
@@ -120,30 +119,30 @@ function view_info(o)
 end
 
 function setup_wire()
-	if hunter ==nil then
-		print("hunter is nil.")
-		return false
-	end
-	-- local playerData=sdk.get_native_field(hunter,targetTypeDef,"_refPlayerData")
-
+	print("setup wire")
 	local playerCommon=hunter:get_field("_PlayerUserDataCommon")
 	-- wire -> playeruserdatacommon
 
-	local speed=2
-	playerCommon:set_field('_HunterWireJumpHighSpeed',5)
+	local speed=0.75
+	playerCommon:set_field('_HunterWireJumpHighSpeed',2)
 	playerCommon:set_field('_HunterWireJumpHighFlySpeed',speed)
 	playerCommon:set_field('_HunterWireJumpJumpLowSpeed',speed)
 	playerCommon:set_field('_HunterWireJumpLowFlySpeed',speed)
-	-- playerCommon:set_field('_HunterWireJumpTargetSpeed',--speed)
+	playerCommon:set_field('_HunterWireJumpTargetSpeed',speed)
 	playerCommon:set_field('_HunterWireJumpTargetFlySpeed',speed)
-	-- playerCommon:set_field('_LongJumpSpeed',speed)
-	print('set wire')
-
-	return true
+	local gravity=-0.012
+	playerCommon:set_field('_HunterWireJumpHighGravity',-0.007)
+	playerCommon:set_field('_HunterWireJumpHighFlyGravity',gravity)
+	playerCommon:set_field('_HunterWireJumpJumpLowGravity',gravity)
+	playerCommon:set_field('_HunterWireJumpLowFlyGravity',gravity)
+	playerCommon:set_field('_HunterWireJumpTargetGravity',gravity)
+	playerCommon:set_field('_HunterWireJumpTargetFlyGravity',gravity)
 end
 
+
 local data=nil
-function data_update()
+function moneyUpdate()
+	print('set money')
 	if data == nil then
 		data=sdk.get_managed_singleton("snow.data.DataManager")
 	end
@@ -152,15 +151,22 @@ function data_update()
 	end
 	local money=data:get_field("_HandMoney")
 	money:set_field("_Value",90000000)
-	return 
+end
+function villagePointUpdate()
+	print('update vp')
+	if data == nil then
+		data=sdk.get_managed_singleton("snow.data.DataManager")
+	end
+	if data==nil then
+		return 
+	end
+	local money=data:get_field("<VillagePointData>k__BackingField")
+	money:set_field("_Point",90000000)
 end
 
 function override_skill()
-	
-	if hunter ==nil then
-		print("hunter is nil.")
-		return false
-	end
+	-- testing
+	print('override skill')
 	local skillList=hunter:get_field("_refPlayerSkillList")
 	local skillData=skillList:get_field("_PlayerSkillData")
 	-- print(skillData)
@@ -175,12 +181,40 @@ function override_skill()
 	
 end
 
--- TODO イベントのハッカが早すぎる。
 
-sdk.hook(sdk.find_type_definition("snow.player.PlayerManager"):get_method("set_PlayerData"), nil, setup_hunter)
-sdk.hook(sdk.find_type_definition("snow.player.PlayerUserDataCommon"):get_method("get_ReffUserDataCommon"), nil, setup_wire)
-sdk.hook(sdk.find_type_definition("snow.player.PlayerBase"):get_method("update"), nil, on_post_update)
-sdk.hook(sdk.find_type_definition("snow.player.PlayerData"):get_method("set__vital"), nil, on_post_vital)
-sdk.hook(sdk.find_type_definition("snow.data.HandMoney"):get_method("addMoney"), nil, data_update)
--- sdk.hook(sdk.find_type_definition("snow.player.PlayerSkillList"):get_method("get_PlayerSkillData"),nil,override_skill)
+function questStart()
+	print("start quest")
+	quest_started=true
+	hunter_initialized=false
+end
+
+function questEnd()
+	print("end quest")
+	quest_started=false
+	hunter_initialized=false
+
+end
+
+function initQuest()
+	print("init quest")
+	quest_started=false
+	hunter_initialized=false
+end
+
+
+-- function test() 
+-- 	quest_started=true
+-- 	hunter_initialized=false
+-- 	updateHunterInfo()
+-- end
+-- test()
+
+sdk.hook(sdk.find_type_definition("snow.QuestManager"):get_method("questStart"), nil, questStart)
+sdk.hook(sdk.find_type_definition("snow.QuestManager"):get_method("onQuestEnd"), nil, questEnd)
+sdk.hook(sdk.find_type_definition("snow.QuestManager"):get_method("initQuestParam"), nil, initQuest)
+
+sdk.hook(sdk.find_type_definition("snow.player.PlayerBase"):get_method("update"), nil, updateStatus)
+
+sdk.hook(sdk.find_type_definition("snow.data.HandMoney"):get_method("addMoney"), nil, moneyUpdate)
+sdk.hook(sdk.find_type_definition("snow.data.VillagePoint"):get_method("setPointSnapshot"), nil, villagePointUpdate)
 
